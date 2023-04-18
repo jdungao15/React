7d41d413e54dce5d127cdfefc648da5e43373de4
John Miguel Dungao <johnmiguel.dungao@gmail.com>
2023-04-17 23:08:45 -0400

    first commit

---
+++ src/dev/previews.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {ComponentPreview, Previews} from '@react-buddy/ide-toolbox'
+import {PaletteTree} from './palette'
+import ExpenseItem from "../components/ExpenseItem";
+
+const ComponentPreviews = () => {
+    return (
+        <Previews palette={<PaletteTree/>}>
+            <ComponentPreview path="/ExpenseItem">
+                <ExpenseItem/>
+            </ComponentPreview>
+        </Previews>
+    )
+}
+
+export default ComponentPreviews
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom/client';
+
+import './index.css';
+import App from './App';
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews, useInitial} from "./dev";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<DevSupport ComponentPreviews={ComponentPreviews}
+                        useInitialHook={useInitial}
+>
+    <App/>
+</DevSupport>);
